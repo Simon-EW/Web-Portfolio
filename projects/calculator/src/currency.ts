035fd041eec42742579771457022e6608d9eacd3
Simon EW <simon.erikssonwestlund@elev.ga.ntig.se>
2022-11-20 23:57:31 +0100

    Calculator Done

--- projects/calculator/src/currency.ts
+++ projects/calculator/src/currency.ts
@@ -1,11 +1,13 @@
 let symbols: { [key: string]: string } = {};
 let rates: { [key: string]: number } = {};
-let lastUpdated = 0;
 let baseCurrency: string;
 
+// Link to the api
 const baseLink = 'https://api.apilayer.com/exchangerates_data/';
+// API key
 const headers = { apiKey: 'BuAuTBowDnl7VX3q7gX7c4Txpg69s6jj' };
 
+// Fetch the currency symbols from the api
 const getCurrencySymbols = async () => {
   try {
     const data = await fetch(baseLink + 'symbols', { headers });
@@ -19,6 +21,7 @@ const getCurrencySymbols = async () => {
   }
 };
 
+// Fetch the rates from the api
 const getCurrencyRates = async () => {
   try {
     const data = await fetch(baseLink + `latest?base=${baseCurrency}`, { headers });
@@ -30,10 +33,6 @@ const getCurrencyRates = async () => {
   } catch (e) {
     throw new Error('Failed to get currency rates');
   }
-};
-
-const getCurrencyFromCode = (code: string) => {
-  return symbols[code] || code;
 };
 
 const getRatesFromCode = (code: string) => {
@@ -48,6 +47,7 @@ const getSymbols = () => {
   return symbols;
 };
 
+// Convert the amount from one currency to another
 const convert = (amount: number, from: string, to: string) => {
   if (from === to) {
     return amount;
@@ -61,19 +61,18 @@ const convert = (amount: number, from: string, to: string) => {
   return (amount * getRatesFromCode(to)) / getRatesFromCode(from);
 };
 
-const init = async (currency = 'USD') => {
+// Function to initialize the currency converter with the base currency
+// which fetches the symbols and rates and returns the functions available to use
+export const init = async (currency = 'USD') => {
   baseCurrency = currency;
   symbols = await getCurrencySymbols();
   const ratesData = await getCurrencyRates();
   rates = ratesData.rates;
-  lastUpdated = ratesData.timestamp;
-};
 
-export default {
-  init,
-  convert,
-  getCurrencyFromCode,
-  getRatesFromCode,
-  getRates,
-  getSymbols,
+  return {
+    convert,
+    getRatesFromCode,
+    getRates,
+    getSymbols,
+  };
 };
