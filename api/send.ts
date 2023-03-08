cdc3a27ab852bdef185872bd7b400293a141f4e3
Simon EW <simon.erikssonwestlund@elev.ga.ntig.se>
2023-03-08 10:40:15 +0100

    Sync

---
+++ api/send.ts
@@ -0,0 +1,87 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import nodemailer from 'nodemailer';
+import Email from 'email-templates';
+import { z } from 'zod';
+import { resolve } from 'path';
+
+const FormFields = z.object({
+  name: z
+    .string()
+    .min(1, { message: 'Name must not be empty' })
+    .max(100, { message: 'Name must not be longer than 100 characters' }),
+  email: z.string().email(),
+  subject: z
+    .string()
+    .min(1, { message: 'Subject must not be empty' })
+    .max(250, { message: 'Subject must not be longer than 250 characters' }),
+  message: z
+    .string()
+    .min(1, { message: 'Message must not be empty' })
+    .max(5000, { message: 'Message must not be longer than 5000 characters' }),
+});
+
+export default async (request: VercelRequest, response: VercelResponse) => {
+  if (request.method !== 'POST') {
+    response.status(405).json({ success: false });
+    return;
+  }
+
+  const input = FormFields.safeParse(JSON.parse(request.body));
+
+  if (!input.success) {
+    response.status(400).json({ success: false, error: input.error });
+    return;
+  }
+
+  const { name, email, subject, message } = input.data;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mail = new Email({
+    message: {
+      from: `Simon <${process.env.EMAIL_USER}>`,
+    },
+    views: {
+      options: {
+        extension: 'ejs',
+      },
+    },
+    juice: true,
+    juiceResources: {
+      preserveImportant: true,
+      webResources: {
+        relativeTo: resolve('./api'),
+      },
+    },
+  });
+
+  const emailTemplate = await mail.render(resolve('./api/contact.ejs'), {
+    name,
+    email,
+    subject,
+    message,
+  });
+
+  const mailOptions = {
+    from: `Simon <${process.env.EMAIL_USER}>`,
+    to: 'simon.alt.form@gmail.com',
+    subject: `Form submission: ${subject}`,
+    html: emailTemplate,
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+
+  if (!info || !info.accepted || info.accepted.length === 0) {
+    response.status(500).json({ success: false });
+
+    return;
+  }
+
+  response.status(200).json({ success: true });
+};
